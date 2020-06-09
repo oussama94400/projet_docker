3a8e1d9f282f565d3b0ffc77b7ce741d07c5fd59
oussama94400 <oussama94-@hotmail.fr>
2020-06-09 15:30:10 +0200

    Create envoi_csv.py

---
+++ envoi_csv.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import logging
+
+log = logging.getLogger()
+log.setLevel('DEBUG')
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+log.addHandler(handler)
+
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
+
+KEYSPACE = "testkeyspace"
+
+
+def main():
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect()
+
+    log.info("creating keyspace...")
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS %s
+        WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
+        """ % KEYSPACE)
+
+    log.info("setting keyspace...")
+    session.set_keyspace(KEYSPACE)
+
+    log.info("creating table...")
+    session.execute("""
+        CREATE TABLE IF NOT EXISTS mytable (
+            film text,
+            Genre text,
+            studio text,
+            audience int,
+            profie text
+            note int,
+            prix text,
+            annee int
+            PRIMARY KEY (film)
+        )
+        """)
+
+    query = SimpleStatement("""
+        INSERT INTO mytable (thekey, col1, col2)
+        VALUES (%(key)s, %(a)s, %(b)s)
+        """, consistency_level=ConsistencyLevel.ONE)
+
+    prepared = session.prepare("""
+        INSERT INTO mytable (thekey, col1, col2)
+        VALUES (?, ?, ?)
+        """)
+
+    for i in range(10):
+        log.info("inserting row %d" % i)
+        session.execute(query, dict(key="key%d" % i, a='a', b='b'))
+        session.execute(prepared, ("key%d" % i, 'b', 'b'))
+
+    future = session.execute_async("SELECT * FROM mytable")
+    log.info("key\tcol1\tcol2")
+    log.info("---\t----\t----")
+
+    try:
+        rows = future.result()
+    except Exception:
+        log.exception("Error reading rows:")
+        return
+
+    for row in rows:
+        log.info('\t'.join(row))
+
+    session.execute("DROP KEYSPACE " + KEYSPACE)
+
+if __name__ == "__main__":
+    main()
